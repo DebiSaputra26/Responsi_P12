7fa5eede3d517ae85fed75d8f4d725eeaf25dede
DebiSaputra26 <debisaputra945@gmail.com>
2025-07-11 13:45:42 +0700

    frontend dan backend

--- vue-project/src/components/Hero.vue
+++ vue-project/src/components/Hero.vue
@@ -6,7 +6,7 @@
 Halo, Saya <span class="text-red-600">Debi Saputra</span>
 </h1>
 <p class="text-xl text-gray-600 mb-8 animate-slide-in-left" style="animation-delay: 400ms;">
-Mahasiswa Informatika Yang Bersemangat Dalam Pengembangan Web Dan Desain Antarmuka.
+Mahasiswa Informatika yang bersemangat dalam dunia pengembangan web dan desain antarmuka, dengan minat tinggi terhadap pembuatan solusi digital yang inovatif, responsif, dan user-friendly. Memiliki ketertarikan pada kombinasi antara logika pemrograman dan estetika desain, serta berkomitmen untuk terus belajar dan berkembang dalam menciptakan pengalaman pengguna yang optimal melalui teknologi web modern..
 </p>
 
 <a href="#kontak" class="bg-red-600 text-white font-bold py-3 px-6 rounded-lg hover:bg-blue-
